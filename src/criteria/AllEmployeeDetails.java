c72ddb788504ab8abc37fe2399e05ecdab2501e5
tanu <24976766+tanusgit@users.noreply.github.com>
2020-04-03 16:34:22 -0700

    session

--- src/criteria/AllEmployeeDetails.java
+++ src/criteria/AllEmployeeDetails.java
@@ -4,6 +4,9 @@ import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import hibernateFirst.Employee;
 import hibernateFirst.HibernateUtil;
 
@@ -12,7 +15,10 @@ import org.hibernate.criterion.Restrictions;
 
 public class AllEmployeeDetails {
 	public static void main(String[] args) {
-		Session s = HibernateUtil.getSessionFactory().openSession();
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session s = sf.openSession();
+
+		//Session s = HibernateUtil.getSessionFactory().openSession();
 		Criteria empCriteria = s.createCriteria(Employee.class);
 		List<Employee> employees = empCriteria.list();
 		for (Employee e : employees) {
